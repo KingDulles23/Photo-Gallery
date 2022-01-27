d678ddb5c0dd109ec822b7747b9cb2c1a7b0a7ac
King Dulles 23 <2005582@johnabbottcollege.net>
2022-01-27 16:33:25 -0500

    Update recordset-thumbnails.php

--- datasource/recordset-thumbnails.php
+++ datasource/recordset-thumbnails.php
@@ -9,7 +9,7 @@ array (   // the first array inside "content":: child array #1
 		'id'                => "1",
     'metatitle'         => "Pendfield Rooftop",
     'metadescription'   => "Pendfield Rooftop at John Abbott",
-		'h1title'           => "Pendfield Rooftop at John Abbott",
+		'h1title'           => "Pendfield Rooftop at john Abbott",
 		'imagepath'         => "/img/thumbs/002-Addoquaye-Dulles-01-1.jpg",
 		'caption'           => "Portrait of G.W.D student walking inside the Penfield building",
     'copyright'         => "&copy Dulles Addoquaye, 2021"),  // last line ends with a ),
